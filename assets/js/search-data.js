21034e7a6d2c2017467b1a30a409d6ebf063961b
tiffany9056 <39300518+tiffany9056@users.noreply.github.com>
2026-03-19 19:19:39 +0000

    Deploying to gh-pages from @ tiffany9056/tiffany9056.github.io@ba7aa0c7bb84263b0af527a0810c97d9f3402d31 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -57,7 +57,7 @@ ninja.data = [{
           title: 'Rat Seizure Detection &amp;amp; Classification',
           description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/https:/ua-cvml.github.io/";
+              window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
