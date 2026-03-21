69595cd1795886f1858fc3b372b261a34ba4673a
tiffany9056 <39300518+tiffany9056@users.noreply.github.com>
2026-03-21 19:07:12 +0000

    Deploying to gh-pages from @ tiffany9056/tiffany9056.github.io@7c18581e8f5c2c69bbfc31da754d5bc5210ab29f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -91,4 +91,30 @@ ninja.data = [{
           description: "Professor",
           section: "Projects",handler: () => {
               window.location.href = "/projects/xinli_project/";
-            },},];
+            },},{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
